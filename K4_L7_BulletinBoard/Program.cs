44b13a2cd8a7f75f4fd5f18e655478ea65b78da7
Novak Bubanja <bubanjanovak@gmail.com>
2019-03-14 13:04:27 +0100

    kls

--- K4_L7_BulletinBoard/Program.cs
+++ K4_L7_BulletinBoard/Program.cs
@@ -27,6 +27,15 @@ namespace K4_L7_BulletinBoard
         }
     }
 
+    public class Account
+    {
+        [Key]
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    
+    }
+
     public class Artist
     {
         [Key]
