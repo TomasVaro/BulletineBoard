751be393c57d884684c0c1e60d464f82135f81c3
tomasvaro <tomas.varo63@gmail.com>
2019-03-14 13:56:01 +0100

    Made migration and uppdate-database

--- K4_L7_BulletinBoard/Program.cs
+++ K4_L7_BulletinBoard/Program.cs
@@ -13,11 +13,11 @@ namespace K4_L7_BulletinBoard
     {
         public DbSet<Account> Account { get; set; }
         public DbSet<Post> Post { get; set; }
-        public DbSet<Cathegory> Cathegory { get; set; }
+        public DbSet<Category> Cathegory { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
-            options.UseSqlServer(@"Data Source=den1.mssql8.gear.host;Initial Catalog=bulletineboard;Persist Security Info=True;User ID=bulletineboard;Password=***********");
+            options.UseSqlServer(@"Data Source=den1.mssql8.gear.host;Initial Catalog=bulletineboard;Persist Security Info=True;User ID=bulletineboard;Password=Gh80-nc!1lSi");
         }
 
         protected override void OnModelCreating(ModelBuilder model)
@@ -36,7 +36,6 @@ namespace K4_L7_BulletinBoard
     }
 
     public class Post
-
     {
         [Key]
         public int ID { get; set; }
@@ -55,16 +54,6 @@ namespace K4_L7_BulletinBoard
         public string Name { get; set; }
     }
 
-    public class Artist
-    {
-        [Key]
-        public int ID { get; set; }
-        //public string Name { get; set; }
-        //public string Country { get; set; }
-        //public Int16 YearStarted { get; set; }
-        //public List<Album> Albums { get; set; }
-    }
-
     class Program
     {
         static AppDbContext database;
@@ -75,41 +64,43 @@ namespace K4_L7_BulletinBoard
             {
                 while (true)
                 {
-                    string option = ShowMenu("What do you want to do?", new[] {
-                        "List Artists",
-                        "Add New Artist",
-                        "Delete Artist",
-                        "List Albums",
-                        "Add New Album",
-                        "Delete Album",
-                        "List Songs",
-                        "Add New Song",
-                        "Delete Song",
-                        "List Producers",
-                        "Add New Producer",
-                        "Delete Producer",
+                    string option = ShowMenu("Welcome to Bulletin Bored - for when you've got nothing better to do!", new[] {
+                        "Sign In",
+                        "Create Account",
                         "Quit"
                     });
                     Console.Clear();
 
-                    if (option == "List Artists") ListArtists();
-                    else if (option == "Add New Artist") AddNewArtist();
-                    else if (option == "List Albums") ListAlbums();
-                    else if (option == "Delete Artist") DeleteArtist();
-                    else if (option == "Add New Album") AddNewAlbum();
-                    else if (option == "Delete Album") DeleteAlbum();
-                    else if (option == "List Songs") ListSongs();
-                    else if (option == "Add New Song") AddNewSong();
-                    else if (option == "Delete Song") DeleteSong();
-                    else if (option == "List Producers") ListProducers();
-                    else if (option == "Add New Producer") AddNewProducer();
-                    else if (option == "Delete Producer") DeleteProducer();
+                    if (option == "Sign In") SignIn();
+                    //else if (option == "Add New Artist") CreateAccount();
+                    //else if (option == "Create Account") ListAlbums();
                     else Environment.Exit(0);
 
                     Console.WriteLine();
                 }
             }
         }
+
+        static void SignIn()
+        {
+            
+            Console.WriteLine("Write your username:");
+            string username = Console.ReadLine();
+            Console.WriteLine("Write your password:");
+            string password = Console.ReadLine();
+
+            var user = database.Account.Where(a => a.UserName == username && a.Password == password);
+            foreach(var a in user)
+            {
+                Console.WriteLine("You are logged in as " + username);
+            }
+
+            
+            
+        }
+
+
+
 
 
         static string ShowMenu(string prompt, string[] options)
